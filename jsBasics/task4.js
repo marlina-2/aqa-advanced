36422dac5f3662f7421797526de04092cc781942
panchuk-anna <panchuk.anna@airslate.com>
2024-08-09 08:56:34 +0300

    Homework for lesson 4

---
+++ jsBasics/task4.js
@@ -0,0 +1,18 @@
+let circleRadius = 15;
+let circleArea = Math.PI * Math.pow(circleRadius, 2);
+circleArea = circleArea.toFixed(2);
+console.log("Circle area is: ", circleArea);
+
+let length = 13;
+let width = 5
+let rectangleArea = length * width;
+rectangleArea = rectangleArea.toFixed(2);
+console.log("Rectangle area is: ", rectangleArea)
+
+let cylinderRadius = 4;
+let height = 8;
+let cylinderVolume = Math.PI * Math.pow(cylinderRadius, 2) * height;
+cylinderVolume = cylinderVolume.toFixed(2);
+console.log("Cylinder volume is: ", cylinderVolume);
+
+
